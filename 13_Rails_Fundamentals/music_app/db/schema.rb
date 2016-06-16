aa266b6eaf78d0890ab0b1d89c7fa2cc7e521bdb
Jessie Li <jessie@squareup.com>
2016-06-15 18:35:36 -0700

    lesson 14 notes

--- 13_Rails_Fundamentals/music_app/db/schema.rb
+++ 13_Rails_Fundamentals/music_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609034251) do
+ActiveRecord::Schema.define(version: 20160614034259) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20160609034251) do
     t.boolean  "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "track_length"
+    t.integer  "artist_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
